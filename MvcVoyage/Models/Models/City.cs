f4fa4d2da106b20b9099075dc999a8d1cdcb9e77
dvpa666 <42519877+dvpa666@users.noreply.github.com>
2018-08-22 18:11:59 +0200

    Add files via upload

---
+++ MvcVoyage/Models/Models/City.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace MvcVoyage.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        [DisplayName("VoyageType")]
+        public int VoyageTypeId { get; set; }
+        [DisplayName("Country")]
+        public int CountryId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public Country Country { get; set; }
+        public VoyageType VoyageType { get; set; }
+    }
+}
