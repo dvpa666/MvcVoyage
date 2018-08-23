890ad1260546e63582d3f41ec4d26fe6fae9b481
dvpa666 <42519877+dvpa666@users.noreply.github.com>
2018-08-23 14:18:21 +0200

    Add files via upload

---
+++ Controllers/ShoppingCartController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcVoyage.Models;
+using MvcVoyage.ViewModels;
+
+namespace MvcVoyage.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        VoyageEntities storeDB = new VoyageEntities();
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            var viewModel = new ShoppingCartViewModel
+            {
+                CartItems = cart.GetCartItems(),
+                CartTotal = cart.GetTotal()
+            };  
+            return View(viewModel);
+        }
+
+        // GET: /Store/AddToCart/5
+
+        public ActionResult AddToCart(int id)
+        {
+            var addedAlbum = storeDB.Cities.Single(city => city.Id == id);
+
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            cart.AddToCart(addedAlbum);
+
+            return RedirectToAction("Index");
+         }
+
+        [HttpPost]
+        public ActionResult RemoveFromCart(int id)
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            string albumName = storeDB.Carts.Single(item => item.RecordId == id).City.Name;
+
+            int itemCount = cart.RemoveFromCart(id);
+
+            var results = new ShoppingCartRemoveViewModel
+            {
+                Message = Server.HtmlEncode(albumName) +
+                "has been removed from your shopping cart.",
+
+                CartTotal = cart.GetTotal(),
+                CartCount = cart.GetCount(),
+                ItemCount = itemCount,
+                DeleteId = id
+            };
+            return Json(results);
+        }
+
+        [ChildActionOnly]
+        public ActionResult CartSummary()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            ViewData["CartCount"] = cart.GetCount();
+            return PartialView("CartSummary");
+        }
+    }
+}
---
+++ Models/ShoppingCart.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcVoyage.Models
+{
+    public partial class ShoppingCart
+    {
+        VoyageEntities storeDB = new VoyageEntities();
+        string ShoppingCartId { get; set; }
+        public const string CartSessionKey = "CardId";
+        public static ShoppingCart GetCart (HttpContextBase context)
+        {
+            var cart = new ShoppingCart();
+            cart.ShoppingCartId = cart.GetCartId(context);
+            return cart;
+        }
+
+        public static ShoppingCart GetCart(Controller controller)
+            {
+                return GetCart(controller.HttpContext);
+            }
+
+
+        public void AddToCart (City city)
+            {
+                var cartItem = storeDB.Carts.SingleOrDefault(
+                    c => c.CartId == ShoppingCartId
+                    && c.CityId == city.Id);
+
+                if (cartItem == null)
+                    {
+                        cartItem = new Cart
+                        {
+                            CityId = city.Id,
+                            CartId = ShoppingCartId,
+                            Count = 1,
+                            DataCreated = DateTime.Now
+                        };
+                        storeDB.Carts.Add(cartItem);
+                    }
+                else
+                    {
+                        cartItem.Count++;
+                    }
+                storeDB.SaveChanges();
+            }
+
+        public int RemoveFromCart(int id)
+            {
+                var cartItem = storeDB.Carts.Single(
+                    cart => cart.CartId == ShoppingCartId
+                    && cart.RecordId == id);
+
+                int itemCount = 0;
+
+                if (cartItem != null)
+                    {
+                        if (cartItem.Count > 1)
+                            {
+                                cartItem.Count--;
+                                itemCount = cartItem.Count;
+                            }
+                        else
+                            {
+                                storeDB.Carts.Remove(cartItem);
+                            }
+                        storeDB.SaveChanges();
+                    }
+                return itemCount;
+            }
+
+        public void EmptyCart()
+        {
+            var cartItems = storeDB.Carts.Where(
+                cart => cart.CartId == ShoppingCartId);
+
+            foreach (var cartItem in cartItems)
+            {
+                storeDB.Carts.Remove(cartItem);
+            }
+            storeDB.SaveChanges();
+        }
+
+        public List<Cart> GetCartItems()
+            {
+                return storeDB.Carts.Where(
+                    cart => cart.CartId == ShoppingCartId).ToList();
+            }
+        public int GetCount()
+        {
+            //get the count of each item in the cart and sum them up
+            int? count = (from cartItems in storeDB.Carts
+                          where cartItems.CartId == ShoppingCartId
+                          select (int?)cartItems.Count).Sum();
+
+            return count ?? 0;
+        }
+
+        public Decimal GetTotal()
+        {
+            decimal? total = (from cartItems in storeDB.Carts
+                              where cartItems.CartId == ShoppingCartId
+                              select (int?)cartItems.Count *
+                              cartItems.City.Price).Sum();
+
+            return total ?? decimal.Zero;
+        }
+
+        public int CreateOrder(Order order)
+        {
+            decimal orderTotal = 0;
+            var cartItems = GetCartItems();
+
+            foreach (var item in cartItems)
+            {
+                var orderDetail = new OrderDetail
+                {
+                    CityId = item.CityId,
+                    OrderId = order.OrderId,
+                    UnitPrice = item.City.Price,
+                    Quantity = item.Count
+                };
+            }
+                order.Total = orderTotal;
+                storeDB.SaveChanges();
+
+                EmptyCart();
+
+                return order.OrderId;
+        }
+
+        public string GetCartId(HttpContextBase context)
+        {
+            if (context.Session[CartSessionKey] == null)
+            {
+                if (!string.IsNullOrWhiteSpace(context.User.Identity.Name))
+                    {
+                        context.Session[CartSessionKey] =
+                            context.User.Identity.Name;
+                    }
+                else
+                    {
+                        Guid tempCardId = Guid.NewGuid();
+                        context.Session[CartSessionKey] = tempCardId.ToString();
+                    }
+            }
+            return context.Session[CartSessionKey].ToString();
+        }
+
+
+        public void MigrateCart (string userName)
+        {
+            var shoppingCart = storeDB.Carts.Where(
+                c => c.CartId == ShoppingCartId);
+
+            foreach (Cart item in shoppingCart)
+            {
+                item.CartId = userName;
+            }
+
+            storeDB.SaveChanges();
+
+        }
+    }
+}
